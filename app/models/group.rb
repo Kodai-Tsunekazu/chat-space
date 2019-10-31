184666316e42079a3626c75a23767bfd697c0cfa
Kodai-Tsunekazu <p.b.shelley@icloud.com>
2019-10-31 14:08:36 +0900

    group_lastest-message_index

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,19 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+  # def group_user_name
+  #   @group.group_users.each do |group_users|
+  #     = group_users.user.name
+  #   end
+  # end
 end
+
